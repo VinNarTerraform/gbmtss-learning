f9ed82cff5d63e0f2efaa2477940d8a7db6521c5
Vinod Narayanan <133094722+vinodnara@users.noreply.github.com>
2025-06-26 21:23:29 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "mysec1" {
-  ami   = "ami-09e6f87a47903347c"
-  instance_type = "t2.micro"
+  ami   = "ami-09e6f87a47903347c/111111111"
+  instance_type = "t2.micro-111111111"
 }
